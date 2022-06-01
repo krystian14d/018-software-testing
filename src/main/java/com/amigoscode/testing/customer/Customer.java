dba286005584ab4a47db9e32a4816591c5cb1053
Krystian <krystian14d@gmail.com>
2022-06-01 19:56:20 +0200

    SQL and JPA Queries testing

--- src/main/java/com/amigoscode/testing/customer/Customer.java
+++ src/main/java/com/amigoscode/testing/customer/Customer.java
@@ -2,16 +2,22 @@ package com.amigoscode.testing.customer;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 import java.util.UUID;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Customer {
 
+    @Id
     private UUID id;
 
     @NotBlank
--- src/main/java/com/amigoscode/testing/customer/CustomerRegistrationService.java
+++ src/main/java/com/amigoscode/testing/customer/CustomerRegistrationService.java
@@ -1,11 +1,19 @@
 package com.amigoscode.testing.customer;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@AllArgsConstructor
 @Service
 public class CustomerRegistrationService {
 
-    public void registerNewCustomer(CustomerRegistrationRequest request) {
+    private final CustomerRepository customerRepository;
 
+    public void registerNewCustomer(CustomerRegistrationRequest request) {
+        //1. PhoneNumber is taken
+        //2. if taken lets check if belongs to same customer
+        // - 2.1 if yes return
+        // - 2.2 throw an exception
+        //3. Save customer
     }
 }
